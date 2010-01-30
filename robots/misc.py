5eb331d33d25b911f797f69a15c8f4719e5fe207
Michael W <hotdog003@gmail.com>
2010-01-30 00:18:57 -0700

    Added a few good descriptions to pick_cool_name

--- robots/misc.py
+++ robots/misc.py
@@ -9,11 +9,12 @@ def pick_cool_name():
     """
     prefix = ("mega ultra super trady killer spectra power tera kilo giga "
         "yocto proto buzzing sitting hyper canned scrapping astro "
-        "rubber advanced tasty optimized").split()
+        "rubber advanced tasty optimized kingly brave final spinning burning "
+        "quantum illusion tracking mecha").split()
 
     middle = ("planet star chrono duck endurer bot test "
-        "narwhal walrus eating razor spoon typhoon hurricane "
-        "storm opponent foe loser").split()
+        "narwhal walrus eating razor spoon mountain typhoon hurricane "
+        "storm opponent foe loser tracker king royalty mech voltage").split()
 
     suffix = ("killer crusher atomizer shooter v0.1 2.0 9000 1000 3000 1001 "
         "beta alpha prime robot bot buzzer sniper tank juggernaut man "
