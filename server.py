cf70fe72d995c257a4bf5629b388024267d4bc7d
Michael W <hotdog003@gmail.com>
2010-02-06 11:07:50 -0700

    whoo, we can now render jinja templates!

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from twisted.web import server, resource, error, http
+from twisted.web import server, resource, error, http, static
 from twisted.internet import reactor
 from server.custom_factories import JsonTracebackSite
 from server.matches import Matches, Match
+from server import jinja_resource
 
 # Which port to run on?
 PORT = 8080
 
 ROUTES = {
+            '':        jinja_resource.Index(),
+            '/':       jinja_resource.Index(),
+            'index':   jinja_resource.Index(),
             'matches': Matches(),
 }
 
---
+++ server/jinja_resource.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from jinja2 import Environment, PackageLoader
+from twisted.web import resource, server
+
+ENVIRONMENT = Environment(loader=PackageLoader('server', 'static'))
+
+class JinjaResource(resource.Resource):
+    template_name = None
+    variables = {}
+
+    def __init__(self, **kwargs):
+        resource.Resource.__init__(self)
+        self.template = ENVIRONMENT.get_template(self.template_name)
+        self.variables = kwargs
+
+    def render_GET(self, request):
+        request.setHeader("Content-Type", "text/plain; charset=utf-8")
+        request.write(self.template.render(self.variables).encode('utf-8'))
+        request.finish()
+        return server.NOT_DONE_YET
+
+    def getChild(self, path, request):
+        if path == '':
+            # allow trailing slash
+            return self
+        return resource.Resource.getChild(self, path, request)
+
+class Index(JinjaResource):
+    template_name = 'index.htm'
