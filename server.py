0e3ed3f35541ad6bbd632cd4ae2080615be4795d
Michael W <hotdog003@gmail.com>
2010-02-23 20:33:57 -0700

    added a quick IP address display

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from twisted.web import server, resource, error, http, static
+print "Starting up..."
+
+from twisted.web import resource, static
 from twisted.internet import reactor
 from server.custom_factories import JsonTracebackSite
-from server.matches import Matches, Match
+from server.matches import Matches
 from server import jinja_resource
+from server import utils
 
 # Which port to run on?
 PORT = 8080
@@ -14,6 +17,7 @@ ROUTES = {
             '':        jinja_resource.Index(),
             '/':       jinja_resource.Index(),
             'js':      static.File("server/static/js"),
+            # may use clevercss for the following
             'css':     static.File("server/static/css"),
             'img':     static.File("server/static/img"),
             'matches': Matches(),
@@ -26,6 +30,6 @@ for route in ROUTES:
     root.putChild(route, ROUTES[route])
 
 reactor.listenTCP(PORT, JsonTracebackSite(root))
-print "Ready to run"
+print "courier running on http://%s:%d/" % (utils.find_ip(), PORT)
 reactor.run()
 
--- server/utils.py
+++ server/utils.py
@@ -3,6 +3,7 @@
 
 import string
 import random
+import socket
 
 def random_string(size):
     """
@@ -20,3 +21,45 @@ def is_trueish(s):
     if str(s).lower() in ["false", "nil", "null", "0", "no", "f", "n"]:
         return False
     return True
+
+# Utility function to guess the IP (as a string) where the server can be
+# reached from the outside. Quite nasty problem actually.
+# (thanks to woof; the license of this function is as follows)
+#  woof -- an ad-hoc single file webserver
+#  Copyright (C) 2004-2009 Simon Budig  <simon@budig.de>
+# 
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  A copy of the GNU General Public License is available at
+#  http://www.fsf.org/licenses/gpl.txt, you can also write to the
+#  Free Software  Foundation, Inc., 59 Temple Place - Suite 330,
+#  Boston, MA 02111-1307, USA.
+
+def find_ip ():
+   # we get a UDP-socket for the TEST-networks reserved by IANA.
+   # It is highly unlikely, that there is special routing used
+   # for these networks, hence the socket later should give us
+   # the ip address of the default route.
+   # We're doing multiple tests, to guard against the computer being
+   # part of a test installation.
+
+   candidates = []
+   for test_ip in ["192.0.2.0", "198.51.100.0", "203.0.113.0"]:
+      s = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
+      s.connect ((test_ip, 80))
+      ip_addr = s.getsockname ()[0]
+      s.close ()
+      if ip_addr in candidates:
+         return ip_addr
+      candidates.append (ip_addr)
+
+   return candidates[0]
+
