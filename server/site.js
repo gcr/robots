df8ad2093b8cebd9fa1f67bc1266673d5e4c7d4c
Michael W <hotdog003@gmail.com>
2010-03-07 00:16:26 -0700

    Removed extra comment fluff

--- server/site.js
+++ server/site.js
@@ -62,8 +62,10 @@ function genMatchListSite(matches) {
         // Render the match
         // TODO
       },
-      // http://localhost:8080/matches/?register=t
+
+      // no path
       switchboard.dispatchQueryOverloadMega(
+      // http://localhost:8080/matches/?register=t
         ['history'],
         renderHistory(matches.history),
         ['register'],
@@ -89,70 +91,6 @@ function genMatchListSite(matches) {
   }); // end addRoutes
 
 } // end function
-
-    /*//////////// v¯¯ TESTING ¯¯v //////////
-    'test': {
-      // http://localhost:8080/test/a
-      'a': function(req, res) {
-          res.writeHeader(200, {"Content-Type": "text/plain"});
-          res.write("Test A");
-          res.close();
-        },
-      // http://localhost:8080/test/b
-      'b': function(req, res) {
-          res.writeHeader(200, {"Content-Type": "text/plain"});
-          res.write("Test B");
-          res.close();
-        },
-      // http://localhost:8080/test
-      "": function(req, res) {
-          res.writeHeader(200, {"Content-Type": "text/plain"});
-          res.write("Test D+"); res.close();
-        }
-    },
-
-    // This logs stuff when you go into it. A function that dispatches stuff.
-    'log' : function(req, res, path) {
-      log.warn("OHNOES: Somebody is going to " + req.url);
-      return sb.dispatch(req, res, path, {
-        'foo': function(req, res) {
-          res.writeHeader(200, {"Content-Type": "text/plain"});
-          res.write("Foo!\n");
-          res.close();
-        },
-        'bar': function(req, res) {
-          res.writeHeader(200, {"Content-Type": "text/plain"});
-          res.write("Bar!\n");
-          res.close();
-        }});
-    },
-
-    // This section dispatches things based on query strings.
-    'querydispatchtest': sb.dispatchQueryOverloadMega(
-      ['a'],
-      // http://localhost:8080/querydispatchtest?a=...
-      function(req, res, a) {
-        res.writeHeader(200, {"Content-Type": "text/plain"});
-        res.write("A is: " + a);
-        res.close();
-      },
-
-      ['b', 'c'],
-      // http://localhost:8080/querydispatchtest?b=1&c=2
-      function(req, res, b, c) {
-        res.writeHeader(200, {"Content-Type": "text/plain"});
-        res.write("B is: " + b + " and C is: " + c);
-        res.close();
-      },
-
-      [],
-      // http://localhost:8080/querydispatchtest
-      function(req, res) {
-        res.writeHeader(200, {"Content-Type": "text/plain"});
-        res.write("Nothing at all?\n");
-        res.close();
-      })
-      */
 
 
 // Use this function to do things from our HTTP server.
