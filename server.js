17adb1a72f618bd1c901e53d837a709ddaad5620
Michael W <hotdog003@gmail.com>
2010-03-06 21:43:31 -0700

    Separated repl into a different file.
    Added a simple enhancement: the repl will now remember the newest match that's
    been registered.

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ var
   site       = require('site'),
   ears        = require('ears'),
   matchlist   = require('matchlist'),
-  repl        = require('repl'),
-
+  repl        = require('courier_repl'),
   PORT        = 8080;
 
 
@@ -18,20 +17,12 @@ var mlist = new matchlist.MatchList();
 site.genMatchListSite(mlist);
 log.beginLogging();
 
+// Begin our repl
+repl.start(mlist);
+
 // Add all the ears you need above this line
 ears.addEars('MatchList', mlist);
 http.createServer(site.dispatch).listen(PORT);
 log.debug("Started courier on " + Date() + "\nListening on port " + PORT);
 log.info("This server's URL is http://localhost:" + PORT + "/\nGlobals: routes, site, mlist, sys\n\nAt your command.");
 
-// Help our repl out.
-repl.scope.routes = site.routes;
-repl.scope.site = site;
-repl.scope.mlist = mlist;
-repl.scope.sys = require('sys');
-repl.start();
-process.stdio.addListener("close",
-  function() {
-    log.info("Server shutting down.\nLeaving so soon?");
-    process.exit(0);
-  });
---
+++ server/courier_repl.js
@@ -0,0 +1,43 @@
+var
+  repl        = require('repl'),
+  ears        = require('ears'),
+  site        = require('site'),
+  log         = require('log');
+
+function start(mlist) {
+  // Help our repl out.
+
+  process.mixin(repl.scope,
+    {
+      'routes': site.routes,
+      'site': site,
+      'mlist': mlist,
+      'matches': mlist.matches,
+      'sys': require('sys')
+    }
+  );
+
+  repl.prompt = '';
+  repl.start();
+  repl.prompt = 'node> ';
+  process.stdio.addListener("close",
+    function() {
+      log.info("Server shutting down.\nLeaving so soon?");
+      process.exit(0);
+    });
+  ears.listenFor(
+    {'MatchList':
+      {'newMatch':
+        function (match) {
+          repl.scope.newMatch = match;
+        }
+      }
+    }
+  );
+}
+
+process.mixin(exports,
+  {
+    'start': start
+  }
+);
