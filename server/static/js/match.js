6a0e407a9c2e098281fb2cd7a365e4454dc19727
Michael W <hotdog003@gmail.com>
2010-03-23 19:51:44 -0600

    we now use events instead of manually keeping track of our callbacks ourselves

--- server/static/js/match.js
+++ server/static/js/match.js
@@ -14,13 +14,13 @@ function Robot (r) {
     this.armor = r.armor;
     this.heat = r.heat;
   }
-  this.jq = $("<div>");
 }
 
 
 /* ------------------ Match --------------------- */
 function Match(id, authCode) {
   // Represents a match.
+  courier.core.EventEmitter.call(this);
   this.mid = id;
   this.url = "/matches/" + id;
   this.populating = false;
@@ -28,6 +28,8 @@ function Match(id, authCode) {
   this.started = false;
   this.authCode = authCode;
 }
+courier.core.inherits(Match, courier.core.EventEmitter);
+
 Match.prototype.populate = function(stream, cb) {
   // Get information about this match and run the callback when we have it.
   if (this.populating) {
@@ -45,7 +47,6 @@ Match.prototype.populate = function(stream, cb) {
       self.robots = [];
       // the following is a callback. each one should correspond to one slot
       // in self.robots. Be sure to keep track of this and keep it updated!
-      self.onDisconnectRobotCb = [];
       for (var i = 0,l = minfo.robots.length; i < l; i++) {
         self.newSlot();
         if (minfo.robots[i]) {
@@ -82,21 +83,8 @@ Match.prototype.beginStream = function(time) {
         // This handles what to do when the server tells us something.
         switch (get_action(action)) {
           case 'field':
-            if (typeof self.onFieldUpdateCb == 'function') {
-              self.onFieldUpdateCb(action.field);
-            }
-            break;
-          case 'hit':
-            if (typeof self.onHitCb == 'function') {
-              self.onHitCb(action.hit.obj, action.hit.location);
-            }
-            break;
-          case 'splash_damage':
-            if (typeof self.onSplashCb == 'function') {
-              self.onSplashCb(action.splash_damage.objects,
-                            action.splash_damage.location,
-                            action.splash_damage.damage);
-            }
+            // just pass it on
+            self.emit('fieldUpdate', this, action.field);
             break;
           case 'remove_slot':
             self.removeSlot();
@@ -120,6 +108,8 @@ Match.prototype.startMatch = function(cb) {
   // Will try to start the match.
   if (this.authCode && !(this.starting || this.started)) {
     this.starting = true;
+    // no need to call matchStarted; the server may or may not actually decide
+    // to start the match.
     courier.core.ajaxRequest( this.url,
         {start: true, auth_code: this.authCode},
         function() {
@@ -135,26 +125,20 @@ Match.prototype.matchStarted = function() {
   // proper auth code.
   this.started = true;
   this.starting = false;
-  if (typeof this.onMatchStartCb == 'function') {
-    this.onMatchStartCb();
-  }
+  this.emit('matchStarted', this);
 };
 Match.prototype.newSlot = function() {
   // Make a new blank slot.
   this.robots.push(null);
   this.onDisconnectRobotCb.push(null);
-  if (typeof this.onNewSlotCb == 'function') {
-    this.onNewSlotCb();
-  }
+  this.emit('newSlot', this);
 };
 Match.prototype.connectRobot = function(robot) {
   // Add this robot to our next empty slot.
   for (var i=0,l=this.robots.length; i<l; i++) {
     if (this.robots[i] === null) {
       this.robots[i] = robot;
-      if (typeof this.onConnectedRobotCb == 'function') {
-        this.onConnectedRobotCb(this.robots[i]);
-      }
+      this.emit('connectedRobot', this.robots[i]);
       break;
     }
   }
@@ -164,10 +148,7 @@ Match.prototype.disconnectRobot = function(robot) {
   for (var i=0,l=this.robots.length; i<l; i++) {
     if (this.robots[i] && this.robots[i].name == robot.name) {
       this.robots[i] = null;
-      if (typeof this.onDisconnectRobotCb[i] == 'function') {
-        this.onDisconnectRobotCb[i](robot);
-      }
-      this.onDisconnectRobotCb[i] = null;
+      this.emit('disconnectedRobot', robot);
       break;
     }
   }
@@ -179,30 +160,11 @@ Match.prototype.removeSlot = function() {
     if (this.robots[i] === null) {
       this.robots.splice(i, 1);
       this.onDisconnectRobotCb.splice(i, 1);
-      if (typeof this.onRemoveSlotCb == 'function') {
-        this.onRemoveSlotCb();
-      }
+      this.emit('removeSlot', this);
       break;
     }
   }
 };
-Match.prototype.onDisconnectRobot = function(robot, cb) {
-  // Run this callback when this robot is disconnected.
-  for (var i=0,l=this.robots.length; i<l; i++) {
-    if (this.robots[i] && this.robots[i].name == robot.name) {
-      this.onDisconnectRobotCb[i] = cb;
-      break;
-    }
-  }
-};
-courier.core.createPropertySetters(Match, "Cb",
-    ["onFieldUpdate",
-     "onHit",
-     "onSplash",
-     "onConnectedRobot",
-     "onRemoveSlot",
-     "onNewSlot",
-     "onMatchStart"]);
 
 // public methods
 return {
